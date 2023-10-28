4c6918d7ac790ca6cc05b097aeb84f6ed148c529
keikoooooooo <mmhieu.ag@gmail.com>
2023-10-28 19:44:24 +0700

    28 10 2023
    07:44 PM

---
+++ Assets/_Scripts/Game/Enemies/GoblinSlingshot_Effects.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinSlingshot_Effects : MonoBehaviour
+{
+    [Tooltip("Điểm xuất hiện effect")]
+    public Transform effectPoint;
+    
+    
+    
+    
+}
---
+++ Assets/_Scripts/Game/Enemies/GoblinSword_Effects.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinSword_Effects : MonoBehaviour
+{
+    [Tooltip("Điểm xuất hiện effect")]
+    public Transform effectPoint;
+
+    [Tooltip("Script kiểm tra va chạm"), SerializeField]
+    private PhysicsDetection physicsDetection;
+    
+    [Header("Prefab projectile")] 
+    public Reference sword1SlashPrefab;
+    public Reference sword2SlashPrefab;
+
+    public Reference hitPrefab;
+    
+    [Tooltip("Offset góc quay và vị trí xuất hiện effect")] 
+    public List<EffectOffset> effectOffsets;
+    
+    private ObjectPooler<Reference> _poolSword1Slash;
+    private ObjectPooler<Reference> _poolSword2Slash;
+    private ObjectPooler<Reference> _poolHit;
+    private Transform slotVFX;
+
+    
+    private void Start()
+    {
+        slotVFX = new GameObject().transform;
+        _poolSword1Slash = new ObjectPooler<Reference>(sword1SlashPrefab, slotVFX, 5);
+        _poolSword2Slash = new ObjectPooler<Reference>(sword2SlashPrefab, slotVFX, 5);
+        _poolHit = new ObjectPooler<Reference>(hitPrefab, slotVFX, 5);
+    }
+
+
+    private void EffectAttack(AnimationEvent eEvent)
+    {
+        Vector3 _position;    // vị trí effect
+        Quaternion _rotation; // góc quay effect
+
+        switch (eEvent.intParameter)
+        {
+            case 0: 
+                _position = effectPoint.position + transform.rotation *  effectOffsets[0].position;  
+                _rotation = Quaternion.Euler(effectOffsets[0].rotation.x,          
+                                           effectOffsets[0].rotation.y + effectPoint.eulerAngles.y,
+                                             effectOffsets[0].rotation.z);
+                _poolSword1Slash.Get(_position, _rotation);
+                break;
+            
+            case 1:
+                _position = effectPoint.position + transform.rotation *  effectOffsets[1].position;
+                _rotation = Quaternion.Euler(effectOffsets[1].rotation.x, 
+                                           effectOffsets[1].rotation.y + effectPoint.eulerAngles.y,
+                                             effectOffsets[1].rotation.z);
+                _poolSword2Slash.Get(_position, _rotation);
+                break;
+        }
+    }
+    public void EffectHit(Vector3 _position) => _poolHit.Get(_position);
+    
+    
+    public void CheckCollision() => physicsDetection.CheckCollision(); // gọi trên event Animation
+    
+}
